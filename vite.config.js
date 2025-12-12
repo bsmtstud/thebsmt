15b21c9f287738b534fe93acc205c241cc323158
khrissnyder <khrissnyder@gmail.com>
2025-12-12 01:19:12 +0000

    fix: Correctly load PORT environment variable in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -6,27 +6,19 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: true
-  },
-  preview: {
-    port: parseInt(process.env.PORT) || 8080,
-    host: '0.0.0.0'
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    preview: {
+        port: parseInt(env.PORT) || 8080,
+        host: '0.0.0.0'
     },
-    extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json']
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
     },
-  },
-})
+  };
+});
